6237fa765ea60c3a7392c7cc851f3cbc0634d0ab
Joseph Thomas <josephdthomas@google.com>
2024-02-28 22:34:36 +0000

    Hardcode to version 2.6.0

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "test-vpc-module" {
   source       = "terraform-google-modules/network/google"
-  version      = "~> 2.6.0"
+  version      = "2.6.0"
   project_id   = var.project_id 
   network_name = var.network_name 
 
