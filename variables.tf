207b60b2078dfdf7d9d3766c6bebb97b0de466e2
Joseph Thomas <josephdthomas@google.com>
2024-02-28 22:20:20 +0000

    Add basic VPC example

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "project_id" {
+  description = "The ID of the project where this VPC will be created"
+  type        = string
+}
+
+variable "network_name" {
+  description = "The name of the network being created"
+  type        = string
+}
