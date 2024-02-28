80aeec3764756458f0e69a76c13bb0def51074ac
Joseph Thomas <josephdthomas@google.com>
2024-02-28 22:31:05 +0000

    Downgrade to version 2.6.0
    Needed to support Terraform 1.2.3

--- main.tf
+++ main.tf
@@ -1,9 +1,8 @@
 module "test-vpc-module" {
   source       = "terraform-google-modules/network/google"
-  version      = "~> 9.0"
+  version      = "~> 2.6.0"
   project_id   = var.project_id 
   network_name = var.network_name 
-  mtu          = 1460
 
   subnets = [
     {
